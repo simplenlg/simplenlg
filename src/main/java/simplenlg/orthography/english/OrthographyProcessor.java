ff5470e08566ea32880ee30a844e413461197476
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2015-07-08 21:43:16 -0400

    don't allow two commas next to each other

--- src/main/java/simplenlg/orthography/english/OrthographyProcessor.java
+++ src/main/java/simplenlg/orthography/english/OrthographyProcessor.java
@@ -263,6 +263,7 @@ public class OrthographyProcessor extends NLGModule {
 
 			if(realisation != null) {
 				realisation = realisation.replaceAll(" ,", ",");
+				realisation = realisation.replaceAll(",,+", ",");
 				realisedElement.setRealisation(realisation);
 			}
 
